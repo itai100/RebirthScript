65ce314a9d81bf001d922bd3440b02e001c4811d
itai100 <88856816+itai100@users.noreply.github.com>
2026-03-29 23:41:55 +0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,181 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "Rebirth Mania Script 🎁",
+   LoadingTitle = "Itay Hub",
+   LoadingSubtitle = "by Itay",
+   ConfigurationSaving = {
+      Enabled = false,
+      FolderName = nil,
+      FileName = "Big Hub"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true
+   },
+   KeySystem = true,
+   KeySettings = {
+      Title = "Rebirth Mania | key 🔑 ",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key",
+      SaveKey = true,
+      GrabKeyFromSite = true,
+      Key = {"https://pastebin.com/raw/WXng6d0M"}
+   }
+})
+
+local MainTab = Window:CreateTab("🎁Home", nil)
+local MainSection = MainTab:CreateSection("Teleport")    
+
+Rayfield:Notify({
+   Title = "You executed the script",
+   Content = "GL",
+   Duration = 5,
+   Image = nil,
+   Actions = {
+      Ignore = {
+         Name = "Okay!",
+         Callback = function()
+            print("The user tapped Okay!")
+         end
+      },
+   },
+})    
+
+-- 🌍 TELEPORTS
+
+MainTab:CreateButton({
+   Name = "Teleport VIP",
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(125.7, 63.8, 6467.7)
+   end,
+})
+
+MainTab:CreateButton({
+   Name = "Teleport Spawn",
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(90, 39, 6038.4)
+   end,
+})
+
+MainTab:CreateButton({
+   Name = "Teleport Forest",
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(736.3, -26, 4696)
+   end,
+})
+
+MainTab:CreateButton({
+   Name = "Teleport Candy",
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(793.1, 29.8, 5098.8)
+   end,
+})
+
+MainTab:CreateButton({
+   Name = "Teleport Beach",
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(774.4, 30.5, 5396)
+   end,
+})
+
+MainTab:CreateButton({
+   Name = "Teleport Volcanic",
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(766.2, 30, 5708)
+   end,
+})
+
+MainTab:CreateButton({
+   Name = "Teleport Darkness",
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(785, 30, 5975)
+   end,
+})
+
+MainTab:CreateButton({
+   Name = "Teleport Lucid",
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(751, 93, 6258)
+   end,
+})
+
+MainTab:CreateButton({
+   Name = "Teleport Hacker",
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(769, 30, 6738)
+   end,
+})
+
+MainTab:CreateButton({
+   Name = "Teleport Void",
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(763, 30, 7020)
+   end,
+})
+
+-- 🧪 POTIONS TAB (תיקון + הוספה)
+local PotionTab = Window:CreateTab("🧪Potion", nil)
+local PotionSection = PotionTab:CreateSection("Potions")
+
+PotionTab:CreateButton({
+   Name = "INF 5X SECRET",
+   Callback = function()
+       game:GetService("ReplicatedStorage")
+       :WaitForChild("PotionEvent")
+       :WaitForChild("GodPotion")
+       :FireServer(1000000000000001900)
+   end,
+})
+
+PotionTab:CreateButton({
+   Name = "INF 3X TAPS",
+   Callback = function()
+       game:GetService("ReplicatedStorage")
+       :WaitForChild("PotionEvent")
+       :WaitForChild("TapPotion")
+       :FireServer(99999996451)
+   end,
+})
+
+PotionTab:CreateButton({
+   Name = "INF 3X GEMS",
+   Callback = function()
+       game:GetService("ReplicatedStorage")
+       :WaitForChild("PotionEvent")
+       :WaitForChild("GemsPotion")
+       :FireServer(99999999999999680)
+   end,
+})
+
+PotionTab:CreateButton({
+   Name = "INF 2X LUCK",
+   Callback = function()
+       game:GetService("ReplicatedStorage")
+       :WaitForChild("PotionEvent")
+       :WaitForChild("LuckPotion")
+       :FireServer(99999999999999995187)
+   end,
+})
+
+PotionTab:CreateButton({
+   Name = "INF 3X LUCK",
+   Callback = function()
+       game:GetService("ReplicatedStorage")
+       :WaitForChild("PotionEvent")
+       :WaitForChild("LuckPotion2")
+       :FireServer(99999999999999995113)
+   end,
+})
+
+PotionTab:CreateButton({
+   Name = "INF 4X LUCK",
+   Callback = function()
+       game:GetService("ReplicatedStorage")
+       :WaitForChild("PotionEvent")
+       :WaitForChild("LuckPotion3")
+       :FireServer(1000000000871)
+   end,
+})
