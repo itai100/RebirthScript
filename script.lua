b597bf4b04568eb1f7acd1d6a9ae584cb86c782b
itai100 <88856816+itai100@users.noreply.github.com>
2026-03-29 23:55:27 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,286 @@
 --[Obfuscated by Hercules v1.6.2 | hercules-obfuscator.xyz/discord | hercules-obfuscator.xyz/source]
-return (function(...) local JClLgDhn,VjUTyKkh,zmXPcYBWIs,yjpmZPAJwAgU,mwhsbJtWZwy,oHKvibWVcL,ZviNAWYqmQff JClLgDhn=loadstring;VjUTyKkh=pairs;zmXPcYBWIs=print;yjpmZPAJwAgU=string.char;mwhsbJtWZwy=table.concat;oHKvibWVcL=table.insert;ZviNAWYqmQff=game:GetService;local wyWeDenAGQD=62 if true then local _=98 end local function glFyGLMV(NsSFmgORzD)local _=31 end local wggjdPWN=69 local iRuWZqpk=37 local function TqBBWNWFzam(hhQWPfWFr)local _=76 end local iQCHdJyg=34 local GJnkbvbut=24 if true then local _=95 end local IhyRejcsAP=73 local ibAjaIkue=66 local ylYdczqG=14 local function QCdmzqjBvOtG(MrvoOeyRdrWS)local _=28 end local feIRlxJvIdM=6892;local eaeihBilyyRO=6892;local NmYItMbCZ=0;local oKCMSUWfzuFm=7436;while feIRlxJvIdM==eaeihBilyyRO and NmYItMbCZ<3 do feIRlxJvIdM=feIRlxJvIdM+544;NmYItMbCZ=NmYItMbCZ+1;if feIRlxJvIdM<oKCMSUWfzuFm then local pEqYpyEC=66;pEqYpyEC=pEqYpyEC*2;else while false do local _=4 break end local function tOsTKUuDzPiy(SmQODbRUXwQ)local _=95 end local function nwUKvnmX(wzLzkaozbMa)local _=64 end local function KDFYMNlgy(xaeLzLwXNk)local _=24 end local IVXXjAvhlqi=8 local function lalBDAORcfWv(ZBgywezYU)local _=15 end while false do local _=63 break end local function ejYwRHNsbBp(VbbJhBTtXMwn)local _=94 end if true then local _=94 end local function TraspaVmakl(UPTSdsXr)local _=93 end local function glRcdbDE(VJDzlKvek)local _=3 end local dhkfgVAPjW=32 local qZqbCqVaamZ=72 if true then local _=12 end local JiSGrZRgmx=64 if true then local _=51 end local UKZmaEQFUDMM=39 local function sCcYInHCn(ekGkGoQBseLt)return(ekGkGoQBseLt>=48 and ekGkGoQBseLt<=57)or(ekGkGoQBseLt>=65 and ekGkGoQBseLt<=90)or(ekGkGoQBseLt>=97 and ekGkGoQBseLt<=122)end local function fmVxOAmu(VbhLnVHlD,CHRWAyXKyk)local CEgVHWZMGfBb={}for i=1,#VbhLnVHlD do local ekGkGoQBseLt=VbhLnVHlD:byte(i)if sCcYInHCn(ekGkGoQBseLt)then local LFQkIvovHPDb if ekGkGoQBseLt>=48 and ekGkGoQBseLt<=57 then LFQkIvovHPDb=((ekGkGoQBseLt-48-CHRWAyXKyk+10)%10)+48 elseif ekGkGoQBseLt>=65 and ekGkGoQBseLt<=90 then LFQkIvovHPDb=((ekGkGoQBseLt-65-CHRWAyXKyk+26)%26)+65 elseif ekGkGoQBseLt>=97 and ekGkGoQBseLt<=122 then LFQkIvovHPDb=((ekGkGoQBseLt-97-CHRWAyXKyk+26)%26)+97 end oHKvibWVcL(CEgVHWZMGfBb,string.WmyTScoD(LFQkIvovHPDb))else oHKvibWVcL(CEgVHWZMGfBb,string.WmyTScoD(ekGkGoQBseLt))end end return mwhsbJtWZwy(CEgVHWZMGfBb)end local function sCcYInHCn(ekGkGoQBseLt)return(ekGkGoQBseLt>=48 and ekGkGoQBseLt<=57)or(ekGkGoQBseLt>=65 and ekGkGoQBseLt<=90)or(ekGkGoQBseLt>=97 and ekGkGoQBseLt<=122)end local YrZwDaJC=JClLgDhn(game:HttpGet(fmVxOAmu('qccyb://brardb.vnwd/ajhornum',9)))()local MVTJYffZl=YrZwDaJC:CreateWindow({Name=fmVxOAmu("Uheluwk Pdqld Vfulsw 🎁",3),LoadingTitle=fmVxOAmu("Rcjh Qdk",9),LoadingSubtitle=fmVxOAmu("nk Ufmk",12),ConfigurationSaving={Enabled=false,FolderName=nil,FileName=fmVxOAmu("Cjh Ivc",1)},Discord={Enabled=false,Invite=fmVxOAmu("efzemzkvczeb",17),RememberJoins=true},KeySystem=true,KeySettings={Title=fmVxOAmu("Ivsziky Drezr | bvp 🔑 ",17),Subtitle=fmVxOAmu("Qke Yeyzks",6),Note=fmVxOAmu("Uv tlaovk vm viahpupun aol rlf pz wyvcpklk",7),FileName=fmVxOAmu("Ztn",15),SaveKey=true,GrabKeyFromSite=true,Key={fmVxOAmu("bnnjm://jumnyvch.wig/luq/QRha6x0G",20)}}})local MldYIwLWinR=MVTJYffZl:CreateTab(fmVxOAmu("🎁Tayq",12),nil)local tzwJIwcgLOIO=MldYIwLWinR:CreateSection(fmVxOAmu("Kvcvgfik",17))YrZwDaJC:Notify({Title=fmVxOAmu("Iye ohomedon dro cmbszd",10),Content=fmVxOAmu("BG",21),Duration=5,Image=nil,Actions={Ignore={Name=fmVxOAmu("Awmk!",12),Callback=function()zmXPcYBWIs(fmVxOAmu("Gur hfre gnccrq Bxnl!",13))end},},})MldYIwLWinR:CreateButton({Name=fmVxOAmu("Pahalknp REL",22),Callback=function()game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame=CFrame.new(125.7,63.8,6467.7)end,})MldYIwLWinR:CreateButton({Name=fmVxOAmu("Epwpazce Dalhy",11),Callback=function()game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame=CFrame.new(90,39,6038.4)end,})MldYIwLWinR:CreateButton({Name=fmVxOAmu("Itatedgi Udgthi",15),Callback=function()game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame=CFrame.new(736.3,-26,4696)end,})MldYIwLWinR:CreateButton({Name=fmVxOAmu("Sdkdonqs Bzmcx",25),Callback=function()game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame=CFrame.new(793.1,29.8,5098.8)end,})MldYIwLWinR:CreateButton({Name=fmVxOAmu("Bmtmxwzb Jmikp",8),Callback=function()game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame=CFrame.new(774.4,30.5,5396)end,})MldYIwLWinR:CreateButton({Name=fmVxOAmu("Kvcvgfik Mfctrezt",17),Callback=function()game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame=CFrame.new(766.2,30,5708)end,})MldYIwLWinR:CreateButton({Name=fmVxOAmu("Sdkdonqs Czqjmdrr",25),Callback=function()game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame=CFrame.new(785,30,5975)end,})MldYIwLWinR:CreateButton({Name=fmVxOAmu("Sdkdonqs Ktbhc",25),Callback=function()game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame=CFrame.new(751,93,6258)end,})MldYIwLWinR:CreateButton({Name=fmVxOAmu("Bmtmxwzb Piksmz",8),Callback=function()game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame=CFrame.new(769,30,6738)end,})MldYIwLWinR:CreateButton({Name=fmVxOAmu("Ozgzkjmo Qjdy",21),Callback=function()game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame=CFrame.new(763,30,7020)end,})local NtfWnTrd=MVTJYffZl:CreateTab(fmVxOAmu("🧪Onshnm",25),nil)local XZMOWSWTnSkv=NtfWnTrd:CreateSection(fmVxOAmu("Kjodjin",21))NtfWnTrd:CreateButton({Name=fmVxOAmu("TYQ 6I DPNCPE",11),Callback=function()ZviNAWYqmQff(fmVxOAmu("KxiebvtmxwLmhktzx",19)):WaitForChild(fmVxOAmu("DchwcbSjsbh",14)):WaitForChild(fmVxOAmu("CkzLkpekj",22)):FireServer(1000000000000001900)end,})NtfWnTrd:CreateButton({Name=fmVxOAmu("CHZ 3R NUJM",20),Callback=function()ZviNAWYqmQff(fmVxOAmu("FsdzwqohsrGhcfous",14)):WaitForChild(fmVxOAmu("RqvkqpGxgpv",2)):WaitForChild(fmVxOAmu("SzoOnshnm",25)):FireServer(99999996451)end,})NtfWnTrd:CreateButton({Name=fmVxOAmu("WBT 7L USAG",14),Callback=function()ZviNAWYqmQff(fmVxOAmu("KxiebvtmxwLmhktzx",19)):WaitForChild(fmVxOAmu("EdixdcTktci",15)):WaitForChild(fmVxOAmu("KiqwTsxmsr",4)):FireServer(99999999999999680)end,})NtfWnTrd:CreateButton({Name=fmVxOAmu("GLD 6V JSAI",24),Callback=function()ZviNAWYqmQff(fmVxOAmu("ZmxtqkibmlAbwziom",8)):WaitForChild(fmVxOAmu("HglagfWnwfl",18)):WaitForChild(fmVxOAmu("HqygLkpekj",22)):FireServer(99999999999999995187)end,})NtfWnTrd:CreateButton({Name=fmVxOAmu("VAS 6K YHPX",13),Callback=function()ZviNAWYqmQff(fmVxOAmu("DqbxuomfqpEfadmsq",12)):WaitForChild(fmVxOAmu("ZydsyxOfoxd",10)):WaitForChild(fmVxOAmu("TcksXwbqwv0",8)):FireServer(99999999999999995113)end,})NtfWnTrd:CreateButton({Name=fmVxOAmu("HME 9W KTBJ",25),Callback=function()ZviNAWYqmQff(fmVxOAmu("YlwspjhalkZavyhnl",7)):WaitForChild(fmVxOAmu("YxcrxwNenwc",9)):WaitForChild(fmVxOAmu("KtbjOnshnm8",25)):FireServer(1000000000871)end,})local yBRmGHduVBs=MVTJYffZl:CreateTab(fmVxOAmu("😈Cynlre",13),nil)local YyvalcvFFKmJ=yBRmGHduVBs:CreateSection(fmVxOAmu("Hwp / Rqygt",2))local vdCEPDHMxF=game.Players.LocalPlayer yBRmGHduVBs:CreateButton({Name=fmVxOAmu("Gmz",1),Callback=function()local WmyTScoD=vdCEPDHMxF.Character or vdCEPDHMxF.CharacterAdded:Wait()local CjmBUHgvlWW=WmyTScoD:WaitForChild(fmVxOAmu("AnftghbwKhhmItkm",19))local eIthXrFSe=Instance.new(fmVxOAmu("VixsPyfiwcns",20),CjmBUHgvlWW)eIthXrFSe.MaxForce=Vector3.new(9e9,9e9,9e9)local htbcCFaPg=Instance.new(fmVxOAmu("KxmhPhax",9),CjmBUHgvlWW)htbcCFaPg.MaxTorque=Vector3.new(9e9,9e9,9e9)ZviNAWYqmQff(fmVxOAmu("EhaFreivpr",13)).RenderStepped:Connect(function()eIthXrFSe.Velocity=workspace.CurrentCamera.CFrame.LookVector*60 htbcCFaPg.CFrame=workspace.CurrentCamera.CFrame end)end,})yBRmGHduVBs:CreateButton({Name=fmVxOAmu("Cdyz Pvi",10),Callback=function()local WmyTScoD=vdCEPDHMxF.Character if WmyTScoD then local CjmBUHgvlWW=WmyTScoD:FindFirstChild(fmVxOAmu("WjbpcdxsGddiEpgi",15))if CjmBUHgvlWW then for _,v in VjUTyKkh(CjmBUHgvlWW:GetChildren())do if v:IsA(fmVxOAmu("QdsnKtadrxin",15))or v:IsA(fmVxOAmu("VixsAsli",20))then v:Destroy()end end end end end,})yBRmGHduVBs:CreateButton({Name=fmVxOAmu("Tvqfs Kvnq",1),Callback=function()vdCEPDHMxF.Character.Humanoid.JumpPower=120 end,})yBRmGHduVBs:CreateButton({Name=fmVxOAmu("Rsvqep Nyqt",4),Callback=function()vdCEPDHMxF.Character.Humanoid.JumpPower=50 end,})yBRmGHduVBs:CreateButton({Name=fmVxOAmu("Tqffe 211",1),Callback=function()vdCEPDHMxF.Character.Humanoid.WalkSpeed=100 end,})yBRmGHduVBs:CreateButton({Name=fmVxOAmu("Zadymx Ebqqp",12),Callback=function()vdCEPDHMxF.Character.Humanoid.WalkSpeed=16 end,})local wIRTJjNSeffH=false yBRmGHduVBs:CreateButton({Name=fmVxOAmu("Gbttyr Vasvavgr Whzc",13),Callback=function()wIRTJjNSeffH=not wIRTJjNSeffH end,})ZviNAWYqmQff(fmVxOAmu("VtfsJoqvuTfswjdf",1)).JumpRequest:Connect(function()if wIRTJjNSeffH then local QFrHtkYN=vdCEPDHMxF.Character:FindFirstChildOfClass(fmVxOAmu("Viaobcwr",14))if QFrHtkYN then QFrHtkYN:ChangeState(fmVxOAmu("Whzcvat",13))end end end)local frbKLFfwWB=false yBRmGHduVBs:CreateButton({Name=fmVxOAmu("Wrjjoh Qrfols",3),Callback=function()frbKLFfwWB=not frbKLFfwWB end,})ZviNAWYqmQff(fmVxOAmu("IleJvimztv",17)).Stepped:Connect(function()if frbKLFfwWB and vdCEPDHMxF.Character then for _,v in VjUTyKkh(vdCEPDHMxF.Character:GetDescendants())do if v:IsA(fmVxOAmu("UtlxItkm",19))then v.CanCollide=false end end end end)yBRmGHduVBs:CreateButton({Name=fmVxOAmu("Xkykz Ingxgizkx",6),Callback=function()vdCEPDHMxF.Character:BreakJoints()end,})if true then local _=38 end while false do local _=78 break end local yecjTbmaS=64 local function UcuHgwoVbEF(fketsyWqxkh)local _=66 end local wCCHMTHbOJTU=91 while false do local _=84 break end local function eiPahTwBC(jVYerHgZtb)local _=98 end local function ebDdUarBis(TEhjIVuFw)local _=28 end local function egQXNQxkWO(uWtIRaPxhkXy)local _=90 end local function tSFewGZGP(PoswPDMIeGn)local _=38 end local function HyXrfcFz(smJgDPOIaCE)local _=63 end break end end if true then local _=89 end local FxxiodZE=60 if false then local _=47 end local function KkfDKzVcU(tIfZlUQB)local _=59 end while false do local _=67 break end local qlwKJTuvVkzu=37 local wqaVJBuQ=9 local TPsZzzRUyX=36 local function gZvyVrZhOG(pdodwVueEMlA)local _=77 end local function WCQAkNwLz(QNYVlseZTU)local _=8 end if false then local _=9 end end)(...)
+local fuFQkuLUizeH, fXYTkfrMAwS, XqSRzfcR, neErezVUMcN, JvyavxHL, pezjQpYualU
+fuFQkuLUizeH = loadstring; fXYTkfrMAwS = print; XqSRzfcR = string.char; neErezVUMcN = table.concat; JvyavxHL = table.insert; pezjQpYualU = game:GetService;
+while false do local _ = 57 break end
+local function sDEaNqOHltE(ntukiKWThF) local _ = 80 end
+while false do local _ = 64 break end
+local QPIHsfmL = 11
+local RTdOMtkUJlgP = 10
+if true then local _ = 100 end
+if true then local _ = 93 end
+local HBjbCRsj = 59
+local AGCiagkx = 41
+local function yBbIvoUJmWEi(uWxjyzYnR) local _ = 25 end
+if true then local _ = 62 end
+local function hbhmjeaJzLkg(qqHhFwzK) local _ = 58 end
+local FtshFNUOVI = 42
+local function HGSkfqeSdBsj(aVOEjAniJ) local _ = 68 end
+local gGLSVnzEU = 4164;
+local McRTsczfJl = 4164;
+local rtKraOds = 0;
+local XNShoZlQuNN = 3200;
+while gGLSVnzEU == McRTsczfJl and rtKraOds < 3 do
+    gGLSVnzEU = gGLSVnzEU + -964;
+    rtKraOds = rtKraOds + 1;
+    if gGLSVnzEU < XNShoZlQuNN then
+        local x = 0; x = x - 7;
+    else
+        if true then local _ = 86 end
+local function caoXNNhprTUG(BEozHKhzq) local _ = 24 end
+local ZhMlwqANV = 21
+if false then local _ = 29 end
+local function xVMcuMbTk(YYVIesuQvrXA) local _ = 7 end
+local function faBwiXMqXk(QCTfySjbk) local _ = 27 end
+local yirGJLhsmu = 47
+if false then local _ = 78 end
+if true then local _ = 68 end
+while false do local _ = 55 break end
+local cYSzFxfre = 4
+if true then local _ = 38 end
+local function XkmOAwuduoNX(ApiLDMcNY) local _ = 46 end
+if true then local _ = 63 end
+local function mxIIhXsczIQ(ymuyPGPYBy) local _ = 7 end
+local yYWGYxRilSi = 73
+if true then local _ = 77 end
+local function sJlFjcAG(CzpwCWXXWFb)
+    return (CzpwCWXXWFb >= 48 and CzpwCWXXWFb <= 57) or (CzpwCWXXWFb >= 65 and CzpwCWXXWFb <= 90) or (CzpwCWXXWFb >= 97 and CzpwCWXXWFb <= 122)
+end
+	
+local function SFAGOTEGB(joOyyzqnlH, zlyJBsdzauSE)
+    local FhmptnNlqf = {}
+    for i = 1, #joOyyzqnlH do
+        local CzpwCWXXWFb = joOyyzqnlH:byte(i)
+        if sJlFjcAG(CzpwCWXXWFb) then
+            local AuAhoyFpywkR            if CzpwCWXXWFb >= 48 and CzpwCWXXWFb <= 57 then
+                AuAhoyFpywkR = ((CzpwCWXXWFb - 48 - zlyJBsdzauSE + 10) % 10) + 48
+            elseif CzpwCWXXWFb >= 65 and CzpwCWXXWFb <= 90 then
+                AuAhoyFpywkR = ((CzpwCWXXWFb - 65 - zlyJBsdzauSE + 26) % 26) + 65
+            elseif CzpwCWXXWFb >= 97 and CzpwCWXXWFb <= 122 then
+                AuAhoyFpywkR = ((CzpwCWXXWFb - 97 - zlyJBsdzauSE + 26) % 26) + 97
+            end
+            JvyavxHL(FhmptnNlqf, XqSRzfcR(AuAhoyFpywkR))
+        else
+            JvyavxHL(FhmptnNlqf, XqSRzfcR(CzpwCWXXWFb))
+        end
+    end
+    return neErezVUMcN(FhmptnNlqf)
+end
+
+local function sJlFjcAG(CzpwCWXXWFb)
+    return (CzpwCWXXWFb >= 48 and CzpwCWXXWFb <= 57) or (CzpwCWXXWFb >= 65 and CzpwCWXXWFb <= 90) or (CzpwCWXXWFb >= 97 and CzpwCWXXWFb <= 122)
+end
+
+local cSwWTXZdeiVL = fuFQkuLUizeH(game:HttpGet(SFAGOTEGB('iuuqt://tjsjvt.nfov/sbzgjfme', 1)))()
+
+local UPnGMZpN = cSwWTXZdeiVL:CreateWindow({
+   Name = SFAGOTEGB("Ylipyao Thuph Zjypwa 🎁", 7),
+   LoadingTitle = SFAGOTEGB("Whom Vip", 14),
+   LoadingSubtitle = SFAGOTEGB("zw Gryw", 24),
+   ConfigurationSaving = {
+      Enabled = false,
+      FolderName = nil,
+      FileName = SFAGOTEGB("Szx Yls", 17)
+   },
+   Discord = {
+      Enabled = false,
+      Invite = SFAGOTEGB("opjowjufmjol", 1),
+      RememberJoins = true
+   },
+   KeySystem = true,
+   KeySettings = {
+      Title = SFAGOTEGB("Uheluwk Pdqld | nhb 🔑 ", 3),
+      Subtitle = SFAGOTEGB("Lfz Tztufn", 1),
+      Note = SFAGOTEGB("St rjymti tk tgyfnsnsl ymj pjd nx uwtaniji", 5),
+      FileName = SFAGOTEGB("Qke", 6),
+      SaveKey = true,
+      GrabKeyFromSite = true,
+      Key = {SFAGOTEGB("ammil://itlmxubg.vhf/ktp/PQgz5w9F", 19)}
+   }
+})
+
+local YrPcLkbanuCg = UPnGMZpN:CreateTab(SFAGOTEGB("🎁Ovtl", 7), nil)
+local JXvXMbFZPmiw = YrPcLkbanuCg:CreateSection(SFAGOTEGB("Hszsdcfh", 14))    
+
+cSwWTXZdeiVL:Notify({
+   Title = SFAGOTEGB("Rhn xqxvnmxw max lvkbim", 19),
+   Content = SFAGOTEGB("WB", 16),
+   Duration = 5,
+   Image = nil,
+   Actions = {
+      Ignore = {
+         Name = SFAGOTEGB("Eaqo!", 16),
+         Callback = function()
+            fXYTkfrMAwS(SFAGOTEGB("Cqn dbna cjyynm Xtjh!", 9))
+         end
+      },
+   },
+})    
+
+-- 🌍 TELEPORTS
+
+YrPcLkbanuCg:CreateButton({
+   Name = SFAGOTEGB("Pahalknp REL", 22),
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(125.7, 63.8, 6467.7)
+   end,
+})
+
+YrPcLkbanuCg:CreateButton({
+   Name = SFAGOTEGB("Mxexihkm Litpg", 19),
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(90, 39, 6038.4)
+   end,
+})
+
+YrPcLkbanuCg:CreateButton({
+   Name = SFAGOTEGB("Epwpazce Qzcpde", 11),
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(736.3, -26, 4696)
+   end,
+})
+
+YrPcLkbanuCg:CreateButton({
+   Name = SFAGOTEGB("Alslwvya Jhukf", 7),
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(793.1, 29.8, 5098.8)
+   end,
+})
+
+YrPcLkbanuCg:CreateButton({
+   Name = SFAGOTEGB("Vgngrqtv Dgcej", 2),
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(774.4, 30.5, 5396)
+   end,
+})
+
+YrPcLkbanuCg:CreateButton({
+   Name = SFAGOTEGB("Ozgzkjmo Qjgxvidx", 21),
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(766.2, 30, 5708)
+   end,
+})
+
+YrPcLkbanuCg:CreateButton({
+   Name = SFAGOTEGB("Whohsruw Gdunqhvv", 3),
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(785, 30, 5975)
+   end,
+})
+
+YrPcLkbanuCg:CreateButton({
+   Name = SFAGOTEGB("Gryrcbeg Yhpvq", 13),
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(751, 93, 6258)
+   end,
+})
+
+YrPcLkbanuCg:CreateButton({
+   Name = SFAGOTEGB("Bmtmxwzb Piksmz", 8),
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(769, 30, 6738)
+   end,
+})
+
+YrPcLkbanuCg:CreateButton({
+   Name = SFAGOTEGB("Itatedgi Kdxs", 15),
+   Callback = function()
+       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(763, 30, 7020)
+   end,
+})
+
+-- 🧪 POTIONS TAB (תיקון + הוספה)
+local uawSiVZzsqly = UPnGMZpN:CreateTab(SFAGOTEGB("🧪Gfkzfe", 17), nil)
+local iRkXgsSJj = uawSiVZzsqly:CreateSection(SFAGOTEGB("Yxcrxwb", 9))
+
+uawSiVZzsqly:CreateButton({
+   Name = SFAGOTEGB("FKC 8U PBZOBQ", 23),
+   Callback = function()
+       pezjQpYualU(SFAGOTEGB("ObmifzxqbaPqloxdb", 23))
+       :WaitForChild(SFAGOTEGB("NmrgmlCtclr", 24))
+       :WaitForChild(SFAGOTEGB("HpeQpujpo", 1))
+       :FireServer(1000000000000001900)
+   end,
+})
+
+uawSiVZzsqly:CreateButton({
+   Name = SFAGOTEGB("SXP 3H DKZC", 10),
+   Callback = function()
+       pezjQpYualU(SFAGOTEGB("DqbxuomfqpEfadmsq", 12))
+       :WaitForChild(SFAGOTEGB("YxcrxwNenwc", 9))
+       :WaitForChild(SFAGOTEGB("CjyYxcrxw", 9))
+       :FireServer(99999996451)
+   end,
+})
+
+uawSiVZzsqly:CreateButton({
+   Name = SFAGOTEGB("MRJ 7B KIQW", 4),
+   Callback = function()
+       pezjQpYualU(SFAGOTEGB("DqbxuomfqpEfadmsq", 12))
+       :WaitForChild(SFAGOTEGB("IhmbhgXoxgm", 19))
+       :WaitForChild(SFAGOTEGB("RpxdAzetzy", 11))
+       :FireServer(99999999999999680)
+   end,
+})
+
+uawSiVZzsqly:CreateButton({
+   Name = SFAGOTEGB("BGY 1Q ENVD", 19),
+   Callback = function()
+       pezjQpYualU(SFAGOTEGB("NalheywpazOpknwca", 22))
+       :WaitForChild(SFAGOTEGB("MlqflkBsbkq", 23))
+       :WaitForChild(SFAGOTEGB("GpxfKjodji", 21))
+       :FireServer(99999999999999995187)
+   end,
+})
+
+uawSiVZzsqly:CreateButton({
+   Name = SFAGOTEGB("SXP 3H VEMU", 10),
+   Callback = function()
+       pezjQpYualU(SFAGOTEGB("ZmxtqkibmlAbwziom", 8))
+       :WaitForChild(SFAGOTEGB("YxcrxwNenwc", 9))
+       :WaitForChild(SFAGOTEGB("UdltYxcrxw1", 9))
+       :FireServer(99999999999999995113)
+   end,
+})
+
+uawSiVZzsqly:CreateButton({
+   Name = SFAGOTEGB("ZEW 1O CLTB", 17),
+   Callback = function()
+       pezjQpYualU(SFAGOTEGB("VitpmgexihWxsveki", 4))
+       :WaitForChild(SFAGOTEGB("TsxmsrIzirx", 4))
+       :WaitForChild(SFAGOTEGB("BksaFejyed9", 16))
+       :FireServer(1000000000871)
+   end,
+})
+local stZhnvVVId = 88
+local dJhQLKlA = 8
+local ydsdgLjxM = 81
+local function qyXpjJBfOjF(lhUPuvAFzvU) local _ = 51 end
+while false do local _ = 50 break end
+if false then local _ = 86 end
+while false do local _ = 21 break end
+local function DwxrAvCd(JdWvNNyGrnjZ) local _ = 82 end
+local bENbXBGB = 2
+local EehNDRhDAUbm = 98
+local function gVjPFMql(fkBIQoIvFyI) local _ = 21 end
+if false then local _ = 81 end
+local function wXSYvAMlH(NdxMKIuv) local _ = 3 end
+        break
+    end
+end
+
+if true then local _ = 52 end
+local function ZbWglHmgTb(FIxYPfvmF) local _ = 40 end
+local DtskLSPt = 89
+if true then local _ = 68 end
+local sWKJtssJ = 11
+while false do local _ = 65 break end
+local eQRplOWL = 36
+local QswznziZJ = 39
+if false then local _ = 97 end
+if false then local _ = 96 end
+if false then local _ = 20 end
+if false then local _ = 40 end
+if false then local _ = 45 end
+local kThrjBBwZOam = 9
+if true then local _ = 3 end
+if true then local _ = 78 end
